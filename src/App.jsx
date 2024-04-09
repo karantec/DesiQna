d728583629941f25742e53f70256fd7605b24291
Shu12388y <spl.sp999@gmail.com>
2024-04-09 23:02:08 +0530

    file addeD

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-
+import { Navbar } from './components/navbar/NavBar';
+import { Footer } from './components/footer/Footer';
+import HeroSection from './components/heroSection/HeroSection';
+import Contact from './components/contact/Contact';
+import {BrowserRouter as Router ,Route, Routes } from 'react-router-dom';
+import Feature from './components/Features/Feature';
+import About from './components/about/About';
 const App = () => {
   return (
-    <div>
-    <h2 className='text-center'>Designa</h2>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>  </div>
+    <>
+    <Router>
+
+
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<HeroSection/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/about' element={<About/>}/>
+
+
+
+    </Routes>
+    <Footer/>
+    </Router>
+
+    </>
   )
 }
 
