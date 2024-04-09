b269e7bc5b1ae173183241eccb6be3456ff5cc02
karanrana <karanrana3095@gmail.com>
2024-04-09 12:27:50 +0530

    desiqnasite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <h2 className='text-center'>Designa</h2>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>  </div>
+  )
+}
+
+export default App
