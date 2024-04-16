19e2e00b221bc62e159576884694ec64702f4546
Shu12388y <spl.sp999@gmail.com>
2024-04-16 21:44:05 +0530

    file added

--- src/components/Features/components/Feature.jsx
+++ src/components/Features/components/Feature.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Feature({p,h1,company,details,img}) {
   return (
   <>
-   <section className="px-24">
+   <section className="mr-5 ml-5 flex flex-col items-center justify-center">
         <section className="text-gray-600 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
