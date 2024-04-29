cc8bd68c294d04416f895116bff68f27913f0a77
karanrana <karanrana3095@gmail.com>
2024-04-29 19:32:42 +0530

    smallbugs

---
+++ Desktop/DesiQna-mainNew/DesiQna-main/src/components/Features/data/data.js
@@ -0,0 +1,110 @@
+export const data={
+    user1:{
+        id: 1,
+        p:"Tier-4 College to 40+ LPA",
+        h1:"Pranav cracks off campus SDE Intern + Fulltime offer from Linkedin(40 LPA CTC) after 8 months of FAANG level DSA(OA + Interview) Training with us!",
+        student:"Pranav Kulkarni",
+        company:"Linkedin",
+        img:'https://media.licdn.com/dms/image/D5603AQEEmONMfPojPA/profile-displayphoto-shrink_400_400/0/1712824265014?e=1719446400&v=beta&t=s4I8aIL1dnu3sX6QItrNhIF_kOEqGsdkOxQgZjqrv6g'
+
+    },
+    user2:{
+        id: 2,
+        p:"5LPA Service based to 30LPA SDE Offer from Flipkart Product Based Company",
+        h1:"Student who was a DSA beginner and was working in service based company cracks 30 LPA Off Campus SDE Offer from Flipkart after 8 months of FAANG level DSA(OA + Interview) Training with us!",
+        student:"Akhil Sharma",
+        company:"Flipkart",
+        img:'https://media.licdn.com/dms/image/C4E03AQFtGkZrZcN3Eg/profile-displayphoto-shrink_400_400/0/1621875901213?e=1719446400&v=beta&t=9Nm3QY6SgZK28v1Z9v0rFD9yd-eFrVauQ1tFQxaE36o'
+
+    },
+    user3:{
+        id: 3,
+        p:"Tier-3 College to 18+ LPA",
+        h1:" Student  cracked 18 lpa SDE Offer Off-campus from product based company Experian ",
+        student:"Sandeep Nuka.",
+        company:"Experian'",
+        img:'niki.jpg'
+
+    },
+    user4:{
+        id: 4,
+        p:"Tier-3 College to Microsoft",
+        h1:" Student cracks Microsoft Off Campus offer  after 8 month FAANG level OA + Interview + CP training",
+        company:"Microsoft",
+        student:"Sejal Aggarwal.",
+        img:'https://media.licdn.com/dms/image/D5603AQHNoXF95qw5ZA/profile-displayphoto-shrink_400_400/0/1711283069934?e=1719446400&v=beta&t=UM-lbOi2FMUa2aVk4AZPzPZhFo6vn-aNf6mHauRzauE'
+
+    },
+    user5:{
+        id: 5,
+        p:"Tier-3 College to 28 LPA SDE Offer OFF Campus",
+        h1:" Student fully beginner at DSA; cracks ZScaler 28 LPA Offer after 5 month FAANG level DSA + OA + Mock Interview Training + 800 hour course! ",
+        student:"Anurag Tiwar",
+        company:"Zscaler",
+        img:'https://media.licdn.com/dms/image/D4D03AQFTe5eTH9gmxg/profile-displayphoto-shrink_400_400/0/1706086206108?e=1718841600&v=beta&t=I2ZWnC0Z5o2TUv2N0Wb0siD3bA7xpCJjBObvSuhTZr8'
+    },
+    user6:{
+        id: 6,
+        p:"Tier-3 College to SDE Offer OFFCampus ClearTrip",
+        h1:" Cracks Flipkart Group(ClearTrip) SDE Offer Off Campus   after training with us for 8 months! ",
+        student:"Vineeta Agarwal.",
+        company:"  ClearTrip",
+        img:'https://media.licdn.com/dms/image/D4D03AQHZRfgJB2V0MQ/profile-displayphoto-shrink_400_400/0/1709395257890?e=1718841600&v=beta&t=I3lDNZqCiweOBLpW5bnC_fQNsU3AUr5OijuJhFaLues'
+    },
+   
+    user8:{
+        id: 7,
+        p:"Tier-3 College to World Rank-50",
+        h1:" Student gets World Rank 50/25000 in LeetCode DSA Contest",
+        student:"Vinit Tekwani ",
+        company:" Hexaview Technologies",
+        img:'https://media.licdn.com/dms/image/D4D03AQHOApMWWJHhMw/profile-displayphoto-shrink_400_400/0/1673364727709?e=1718841600&v=beta&t=aAYB1agZt6WqbCpc2Agm_PVYm-3jbHoLQsNiTglh9V8'
+    },
+    user9:{
+        id: 8,
+        p:"2023 Grad with Gap to Off campus SDE offer at Societe Generale",
+        h1:"Student with 2023 year of graduation cracks off campus SDE offer at Societe Generale; Top PBC despite having a gap of 8+ months. ",
+        student:"Abhishek Mishra",
+        company:"Societe Generale",
+        img:'https://media.licdn.com/dms/image/D5603AQHKcL3j54euhg/profile-displayphoto-shrink_400_400/0/1671459711386?e=1718841600&v=beta&t=_m7dWfDewP3yqyhChiI9Scqil9RDJnkQLcfXuJn1i2w'
+
+    },
+    user10:{
+        id: 9,
+        p:"Service Based to off Campus SDE offer at Ford Motor Company",
+        h1:"Student who got laid cracks off Campus SDE offer from Ford after 6 months training. ",
+        student:"Surya",
+        company:"Ford Motor Company",
+        img:'https://media.licdn.com/dms/image/D5603AQEo4KYq1yCG7g/profile-displayphoto-shrink_400_400/0/1706681234827?e=1718841600&v=beta&t=jzMi7n0QnOr5LXSHkcvMlBAZbiiv-0KRlYYBgURuCx0',
+    },
+    user11:{
+        id: 10,
+        p:"Tier-3 College to 10LPA SDE offer offcampus  from Zoho",
+        h1:"Student with 0 college placements cracks 10LPA SDE offer offcampus  from Zoho . ",
+        student:"Raj Rai",
+        company:"Zoho Corporation",
+        img:'https://media.licdn.com/dms/image/D5603AQEZNo223WMZsg/profile-displayphoto-shrink_400_400/0/1709796930528?e=1718841600&v=beta&t=FtGgqJOug_I2e2qql5k3V2b0WGq1febUaij3dlnwGXc'
+
+
+    },
+   
+    user12:{
+        id: 11,
+        p:"Tier-3 College to OFF Campus SDE Offer ",
+        h1:"Student cracks Goldman Sachs OFF Campus SDE Offer  after daily live training of 8 months. ",
+        student:"Prakriti Chatterjee",
+        company:"Goldman Sachs",
+        img:'https://media.licdn.com/dms/image/D5603AQH5cJISTmqgOg/profile-displayphoto-shrink_400_400/0/1706534638737?e=1718841600&v=beta&t=nWZ5q4duuB_1_Z4Gvu8ocYKBNLZRe9D7wJHVzS79eJI'
+    },
+    
+   
+    user13:{
+        id: 12,
+        p:"Tier-3 College to Off-Campus SDE Offer from Swiggy",
+        h1:"Student from tier-3 college; cracks SDE Offer from Swiggy by training super hard with us for DSA(OA + Interview) + LLD + HLD! ",
+        student:"Sarthak Rohtagi",
+        company:"Swiggy",
+        img:'https://media.licdn.com/dms/image/C5603AQGviqRhdrXT8Q/profile-displayphoto-shrink_400_400/0/1615099078037?e=1718841600&v=beta&t=zoyhes8qLPtAVv1h8qHGRB8hNiSvaW81U-2HQrOsMQg'
+    },
+    
+}
