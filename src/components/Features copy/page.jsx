c3d8b3e2342528b7be9439f92237f1adc4e90b7d
karanrana <karanrana3095@gmail.com>
2024-05-13 02:06:35 +0530

    smallbuggfized

--- src/components/Features copy/page.jsx
+++ src/components/Features copy/page.jsx
@@ -4,6 +4,7 @@ import "./Page.css";
 import "animate.css";
 import { DOMAIN } from "../../env/env";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Page() {
   const [selectedUser, setSelectedUser] = useState({
@@ -69,18 +70,14 @@ function Page() {
           </button>
         ))}
       </div>
-      <a
-      aria-label="links"
-        href="https://docs.google.com/document/d/1O9ph3IoyLv_B6scRQSaMyemShoOgAtnWBGpiw-FZtTs/edit"
-        target="_blank"
-        rel="noopener noreferrer"
+      <Link  to="/achiever"
       >
         <div className="inline-flex flex-col items-center justify-center">
           <button role="button" aria-label="more" className=" mt-4 text-red hover:before:bg-red border-2  rounded-lg border-orange-500  relative h-12 w-40 overflow-hidden bg-white px-3 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-orange-500 before:transition-all before:duration-500 hover:text-white hover:shadow-orange-500 hover:before:left-0 hover:before:w-full">
             <span className="relative z-10 text-2xl">View More</span>
           </button>
         </div>
-      </a>
+      </Link>
     </div>
   );
 }
--- src/components/Recent/data/data.js
+++ src/components/Recent/data/data.js
@@ -2,8 +2,8 @@ export const data={
     
        user0:{
         id: 0,
-        p:" Tier-3 College to  2 Crore International  SDE Offer  at USA ",
-        h1:"Student cracks off-campus offers from Nutanix USA after 8 months of advanced DSA(OA + CP) training",
+        p:" Tier-3 College to  2 Crore CTC International  SDE Offer  at USA ",
+        h1:"Mentoring Student cracks off-campus offer from Nutanix USA after 8 months of advanced DSA(OA + CP) training",
         company: "https://www.logo.wine/a/logo/Nutanix/Nutanix-Logo.wine.svg",
         student:"Yash Singhania",
         img:'https://media.licdn.com/dms/image/D4D03AQECAOh8C6dUpw/profile-displayphoto-shrink_400_400/0/1696362279134?e=1721260800&v=beta&t=X3TFMgwyhxDj_REiAJ5Mk8e7NwhZpou8tIldgu02qsI'
@@ -12,7 +12,7 @@ export const data={
     user1:{
         id: 1,
         p:"Tier-3 College to Google and Atlassian Offcampus",
-        h1:"Student cracks off-campus offers from Google and Atlassian after 8 months of advanced DSA(OA + CP) training",
+        h1:"Mentoring Student cracks off-campus offers from Google and Atlassian after 8 months of advanced DSA(OA + CP) training",
         company: "https://iconape.com/wp-content/files/ur/370111/svg/google-logo-icon-png-svg.png",
         student:"Anusha Yerram",
         img:'https://media.licdn.com/dms/image/D4D03AQF-p3T34OuXUQ/profile-displayphoto-shrink_400_400/0/1709905148028?e=1720051200&v=beta&t=EQmtOxQXITjDjH9Gpp_ikJGuWxTo-XjaaiSj6eafI8I'
